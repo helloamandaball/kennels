1e7613fc12aadce3a8b9474b165363cbe4d81b9d
helloamandaball <helloamandaball@gmail.com>
2021-12-06 16:00:58 -0500

    updates w/ ch. 10

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,25 +1,57 @@
-import React from "react"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationViews"
-import "./Kennel.css"
+import React, { useState } from "react";
+import { NavBar } from "./nav/NavBar";
+import { ApplicationViews } from "./ApplicationViews";
+import "./Kennel.css";
+import { Routes, Route, Navigate } from "react-router";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 
-export const Kennel = () => (
-    <>
+export const Kennel = () => {
+  const [loggedin, setLoggedin] = useState(false);
+
+  const changeState = (bool) => setLoggedin(bool);
+
+  if (localStorage.getItem("kennel_customer")) {
+    return (
+      <>
         <NavBar />
         <ApplicationViews />
-    </>
-)
+      </>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Navigate to="login" />} />
+        <Route path="/login" element={<Login setLoggedin={changeState} />} />
+        <Route path="/register" element={<Register setLoggedin={changeState} />} />
+      </Routes>
+    );
+  }
+};
 
-                                                             
 
-////Previous imports, before Ch. 4
+
+////Previous imports and code, before Ch. 10:
+// import React from "react"
+// import { NavBar } from "./nav/NavBar"
+// import { ApplicationViews } from "./ApplicationViews"
+// import "./Kennel.css"
+
+// export const Kennel = () => (
+//     <>
+//         <NavBar />
+//         <ApplicationViews />
+//     </>
+// )
+
+
+////Previous imports and code, before Ch. 4
 // import { AnimalCard } from "./animal/AnimalCard"
 // import { EmployeeCard } from "./employee/EmployeeCard"
 // import { LocationCard } from "./location/LocationCard"
 // import { CustomerCard } from "./customer/CustomerCard"
 // import { PropsAndState } from "./PropsAndState"
 
-////Previous code, before Ch. 4:
 // export const Kennel = () => (
 //     <>
 //         <h2>Nashville Kennels</h2>
